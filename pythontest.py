2b55e9c775e67fa538dcd87d2d3965cc1a066b81
Cesarm135 <cesarmonino01@colegiomirasur.com>
2024-11-13 14:14:48 +0000

    13/11/24

--- pythontest.py
+++ pythontest.py
@@ -1,5 +1,25 @@
-test = ["a", "b", "c"]
-x, y, z = test
-print(x)
-print(y)
-print(z)
+import json
+print("1: Añadir Tarea, 2: Eliminar Tarea, 3: Ver Tareas")
+userModeInput = input("Seleccionar el modo: ")
+inputTareaAAnadir = ""
+tareaAAnadir = ""
+tareaAAnadirJSON = ""
+
+if (userModeInput == "1"):
+  inputTareaAAnadir = input("Ingresar la tarea: ")
+  tareaAAnadir = {"name": inputTareaAAnadir,
+                  "estado: ": "Pendiente"}
+  tareaAAnadirJSON = json.dumps(tareaAAnadir)
+  f = open("tareas.json", "a")
+  f.write(" \n " + tareaAAnadirJSON)
+  f = open("tareas.json", "r")
+  print("Listo, se ha añadido la tarea: " + inputTareaAAnadir)
+elif (userModeInput == "2"):
+  listNumber = 0
+  f = open("tareas.txt", "r")
+  print
+  
+  
+
+  
+  
