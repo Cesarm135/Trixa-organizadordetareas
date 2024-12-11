c25fbf7af1b9fd8b9444605f717ebe851a20e0e8
PAT0x2345 <120208454+PAT0x23456@users.noreply.github.com>
2024-12-11 13:16:53 +0100

    11/12/24

--- ventana.py
+++ ventana.py
@@ -15,7 +15,9 @@ ventana.configure(bg="white")
 
 frame1 = tk.Frame(ventana)
 frame1.configure(width=300, height=200, bg="red", bd= "5") 
-
+frame2 = tk.Frame(frame1)
+frame2.configure(width=100, height=100, bg="yellow", bd= "5") 
+frame2.pack()
 frame1.pack()
 
 
