74bca770999d121fed6a76d2c296771f6bdac846
PAT0x2345 <120208454+PAT0x23456@users.noreply.github.com>
2024-12-18 13:39:42 +0100

    18/12/24

--- ventana.py
+++ ventana.py
@@ -2,37 +2,23 @@ import tkinter as tk
 from tkinter import PhotoImage
 from pathlib import Path
 import time
-ventana = tk.Tk()
+import customtkinter
 
+ventana = customtkinter.CTk()
+
+# Ajustes Ventana
+#customtkinter.set_appearance_mode("System")
 ventana.title("To-DoList PAI")
-ventana.geometry("540x540")
-ventana.minsize(width=540, height=540)
-icono_ruta = Path(__file__).parent / "Media" / "icono.png"
-icono = PhotoImage(file=str(icono_ruta))
-ventana.iconphoto(False, icono)
-ventana.configure(bg="white")
-
-Numero = 1
-
-etiqueta = tk.Label(ventana, text=Numero)
-etiqueta.configure(fg="black", bg="white", font=("Arial", 34, "bold"))
-etiqueta.after(1000)
-etiqueta.pack()
+customtkinter.set_default_color_theme("blue")
+ventana.configure(fg_color='gray88')
+ventana.geometry("675x375")
+ventana.minsize(width=675, height=375)
+icono_ruta = Path(__file__).parent / "Media" / "icono.ico"
+ventana.after(201, lambda :ventana.iconbitmap(icono_ruta))
+customtkinter.set_appearance_mode("System")
 
 
-
-def botonPress():
-    print("Hola")
-    global Numero
-    Numero += 2
-
-    etiqueta.configure(text=Numero)
-
-boton = tk.Button(text="Hola", bg="green", fg="white", font=("Arial", 24))
-boton.configure(command=botonPress)
-
-
-boton.pack()
-
+entry = customtkinter.CTkEntry(ventana, placeholder_text="Tarea", width="100")
+entry.pack()
 
 ventana.mainloop()
