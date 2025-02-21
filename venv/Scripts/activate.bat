f6351ea6c01e0c6a824a1cea7fe6aa7c41abc99f
PAT0x2345 <120208454+PAT0x23456@users.noreply.github.com>
2025-02-21 21:37:16 +0100

    21/02/2025

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Cms13\Documents\GitHub\CalculadoraPAI\venv"
+set "VIRTUAL_ENV=C:\Users\Cms13\Documents\GitHub\Trixa-organizadordetareas\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
